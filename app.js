78cdc8db19386592bd71ed703817bfeb8231f598
tonycq <tonydev.100@gmail.com>
2025-08-23 11:54:29 -0400

    creacion de la  funcion mostrarDominios para mostrar los dominios en el navegar

--- app.js
+++ app.js
@@ -1,43 +1,70 @@
-let pronombre = ["los", "tus"];
-let adjetivo = ["excelentes", "grandes"];
-let sustantivo = ["Corredores", "desarrolladores"];
-let extensiones = [".com", ".net", ".es", ".io", ".cl"];
-
-function generadorDominios() {
-
+  let pronombre = ["los", "tus"];
+  let adjetivo = ["excelentes", "grandes"];
+  let sustantivo = ["Corredores", "desarrolladores"];
+  let extensiones = [".com", ".net", ".es", ".io", ".cl"];
+  let listaDeDominios =[]  // array vacío para guardar los dominios
 
 
-  for(let p = 0; p< pronombre.length; p++) {
-        
-    for(let a = 0; a < adjetivo.length; a ++) {
-        
-    for(let s = 0; s < sustantivo.length; s ++) {
+  function generadorDominios() {
+
+   listaDeDominios = []; // ← evita duplicados si se vuelve a ejecutar
+
+    for(let p = 0; p< pronombre.length; p++) {
           
-    for(let e = 0; e < extensiones.length; e ++){
-     validarHack(pronombre[p], adjetivo[a], sustantivo[s].toLowerCase(), extensiones[e]);
-       
-  }
+      for(let a = 0; a < adjetivo.length; a ++) {
+          
+      for(let s = 0; s < sustantivo.length; s ++) {
+            
+      for(let e = 0; e < extensiones.length; e ++){
+
+    let dominio = validarHack(
+      pronombre[p], 
+      adjetivo[a], 
+      sustantivo[s].toLowerCase(),
+       extensiones[e]
+      );
+    listaDeDominios.push(dominio); // guardamos el string devuelto
+    
+        
     }
       }
         }
+          }
 
-           }
-          
-  function validarHack (p,a,s,e){
-      
-    let eSinPunto = e.slice(1)
+          return listaDeDominios  // la función devuelve el array
 
-    if (s.endsWith( eSinPunto)){
-let hack = s.slice(0,s.length - eSinPunto.length) + '.'+eSinPunto
- console.log(p+a+hack)
+              }
+  
 
- }else {
-   console.log(p+a+s+e)
-    }
-      
-        }
+    function validarHack (p,a,s,e){
+        
+      let eSinPunto = e.slice(1)
 
-          
+      if (s.endsWith( eSinPunto)){
+  let hack = s.slice(0,s.length - eSinPunto.length) + '.'+eSinPunto
+  console.log(p+a+hack)
+  return p+a+hack   // devolvemos el dominio con hack
+
+  }else {
+    console.log(p+a+s+e)
+    return p+a+s+e // // devolvemos el dominio 
+      }
+        
+          }
+          // esta función se ejecuta al cargar la página
+  document.body.onload = mostrarDominios;
+  function mostrarDominios() {
+    
+    const lista = generadorDominios() // obtencion del array
+    const ul = document.createElement('ul') // creacion de la lista <ul>
+  
+    lista.forEach(dominio => {
+      const li = document.createElement("li"); // cada <li>
+      li.textContent = dominio;                // se agrega el texto
+      ul.appendChild(li);                      // se agrega al ul
+    });
+            
+    document.body.appendChild(ul); // se agrega la lista completa al body
+  }
 
 
-generadorDominios();
